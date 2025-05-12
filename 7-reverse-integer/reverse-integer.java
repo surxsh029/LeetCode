f806a9ac4bc4ae9dcabc21a584cce6ed4fd3fe1c
surxsh029 <kanwasuresh123@gmail.com>
2025-05-12 13:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (93.41%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,12 +3,7 @@ import java.util.*;
 class Solution {
     public int reverse(int x) {
         int ans = 0;
-        int count = 0;
-        if (x < 0) {
-            x = x * (-1);
-            count = 1;
-        }
-        while (x > 0) {
+        while (x != 0) {
             int digit = x % 10;
             x = x / 10;
 
@@ -16,13 +11,7 @@ class Solution {
                 return 0;
             }
             ans = ans * 10 + digit;
-        }
-
-        if (count == 1) {
-            ans = ans * (-1);
-        }
-
-        
+        }       
 
         return ans;
     }
