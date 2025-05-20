9e2a1ab52542cc30973635ccb65bc3338433430c
surxsh029 <kanwasuresh123@gmail.com>
2025-05-20 13:55:18 +0530

    Time: 4 ms (8.59%) | Memory: 83.7 MB (75.86%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -5,18 +5,15 @@ class Solution {
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-
-                boolean top = (i == 0) || (mat[i][j] > mat[i - 1][j]);
-                boolean bottom = (i == rows - 1) || (mat[i][j] > mat[i + 1][j]);
-                boolean left = (j == 0) || (mat[i][j] > mat[i][j - 1]);
-                boolean right = (j == cols - 1) || (mat[i][j] > mat[i][j + 1]);
-
-                if (top && bottom && left && right) {
-                    return new int[]{i, j};
-                }
+                if (i > 0 && mat[i][j] <= mat[i - 1][j]) continue;
+                if (i < rows - 1 && mat[i][j] <= mat[i + 1][j]) continue;
+                if (j > 0 && mat[i][j] <= mat[i][j - 1]) continue;
+                if (j < cols - 1 && mat[i][j] <= mat[i][j + 1]) continue;
+                return new int[]{i, j};
             }
         }
 
-        return new int[]{-1, -1};  
+        return new int[]{-1, -1};
     }
 }
+
