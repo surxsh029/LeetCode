a2b7b9fd862a98a91373b04529fbb072d018d6d2
surxsh029 <kanwasuresh123@gmail.com>
2025-05-22 09:22:38 +0530

    Time: 35 ms (5.45%), Space: 47.7 MB (98.07%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -6,21 +6,18 @@ class Solution {
         int right=Arrays.stream(nums).max().getAsInt();
 
         while(right>left){
+            int mid=left+(right-left)/2;
+            int sum=0;
 
-        
-                int mid=left+(right-left) /2;
-
-                int sum=0;
-                for(int num : nums){
-                    sum+=Math.ceil((double)num/mid);
-                }
-                if(sum>threshold){
-                    left=mid+1;
-                }else{
-                    right=mid;
-                }
+            for(int num : nums){
+                sum+=Math.ceil((double)num/mid);
+            }
+            if(sum>threshold){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
         }
-
         return left;
         
     }
