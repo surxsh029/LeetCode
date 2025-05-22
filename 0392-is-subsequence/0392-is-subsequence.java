c21cb4d620aa3cbf3f3186431f364b74a61705bc
surxsh029 <kanwasuresh123@gmail.com>
2025-05-22 13:29:43 +0530

    Time: 1 ms (93.62%), Space: 42.1 MB (6.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -6,8 +6,7 @@ class Solution {
         for(int i =0;i<s.length();i++){
             for(int j=0;j<t.length();j++){
                 if(s.charAt(i)==t.charAt(j)){
-                    String temp = t.substring(j+1);
-                    t = temp;
+                    t = t.substring(j+1);
                     count++;
                     break;
                 }
