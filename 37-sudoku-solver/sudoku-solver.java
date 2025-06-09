78a780e45e5546bebe9fd9995e11481e85c14f94
surxsh029 <kanwasuresh123@gmail.com>
2025-06-09 09:41:54 +0530

    Time: 158 ms (86.14%) | Memory: 41.2 MB (46.80%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+
+    private boolean solve(char[][] board, int row, int col) {
+        // base case: all rows done
+        if (row == 9) return true;
+
+        int nextRow = row, nextCol = col + 1;
+        if (nextCol == 9) {
+            nextRow = row + 1;
+            nextCol = 0;
+        }
+
+        if (board[row][col] != '.') {
+            return solve(board, nextRow, nextCol);
+        }
+
+        for (char digit = '1'; digit <= '9'; digit++) {
+            if (isSafe(board, row, col, digit)) {
+                board[row][col] = digit;
+
+                if (solve(board, nextRow, nextCol)) {
+                    return true;
+                }
+
+                board[row][col] = '.'; // backtrack
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, char digit) {
+        // row and column check
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == digit || board[i][col] == digit) {
+                return false;
+            }
+        }
+
+        // 3x3 grid check
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
